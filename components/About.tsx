7a9747668bff60a941a444a248543398ff76c1b8
Shenon69 <ask.tikki69@gmail.com>
2023-04-03 07:53:32 +0530

    pages: about

--- components/About.tsx
+++ components/About.tsx
@@ -1,11 +1,55 @@
 import React from 'react'
+import { motion as m } from 'framer-motion'
 
 type Props = {}
 
 export default function About({ }: Props) {
     return (
-        <div className='flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
-            <h3>About</h3>
-        </div>
+        <m.div
+            initial={{
+                opacity: 0
+            }}
+
+            whileInView={{
+                opacity: 1
+            }}
+
+            transition={{
+                duration: 1.2
+            }}
+
+            className='flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
+            <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
+                About
+            </h3>
+            <m.img src="https://avatars.githubusercontent.com/u/105484461?v=4" alt="profile2"
+                initial={{
+                    x: -200,
+                    opacity: 0
+
+                }}
+
+                whileInView={{
+                    x: 0,
+                    opacity: 1
+                }}
+
+                transition={{
+                    duration: 1.2
+                }}
+
+                className='mb-10 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 h-95 lg:w-[250px] lg:h-[300px]'
+            />
+
+            <div className='space-y-10 px-0 md:px-10'>
+                <h4 className='text-4xl font-semibold'>Here is a <span className='underline decoration-[#F7A80A]/50'>little</span>  background</h4>
+                <p text-base>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni
+                    iusto veniam cumque aliquid. Obcaecati nobis voluptatibus iure velit possimus
+                    . Quis perspiciatis culpa corrupti id magni ratione quibusdam ipsam veritatis iste.
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                </p>
+            </div>
+        </m.div>
     )
 }
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -25,7 +25,7 @@ export default function ({ }: Props) {
                 src="https://scontent.fcmb1-2.fna.fbcdn.net/v/t1.6435-9/126618938_173013611135663_878960234911614943_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=oS0xbd7cYqgAX_rJPoc&_nc_ht=scontent.fcmb1-2.fna&oh=00_AfBqSzcgYusIsQ1X60pT6uLuXNm92giFU5DOK54j1RE3Tg&oe=644F0F21"
                 alt="profile" />
 
-            <h2 className='uppercase text-md text-gray-500 pb-2 tracking-[15px] animate-pulse'>Software Engineer</h2>
+            <h2 className='uppercase text-md text-gray-500 pb-2 tracking-[15px] animate-pulse text-center'>Software Engineer</h2>
             <h1 className='text-5xl lg:6xl font-semibold px-10'>
                 <div>
                     <span className='mr-3'>&lt;{text}<Cursor cursorColor='#F7A80A' />/&gt;</span>
